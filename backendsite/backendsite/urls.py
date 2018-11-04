7560d97f1a23d770bce82e0b1c9a9f299750bcee
Unknown <mikeapparel2000-1@yahoo.com>
2018-11-04 07:07:15 +0100

    Update settings to use function imports
    Add the polls as an app to the settings.py file. Modify urls.py to use functions rather than strings.

--- backendsite/backendsite/urls.py
+++ backendsite/backendsite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import polls.views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('polls/', polls.views.index, name='polls'),
     path('admin/', admin.site.urls),
 ]
